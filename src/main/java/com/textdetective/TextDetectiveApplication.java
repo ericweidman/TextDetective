5da4f78b3bb79765634cd97aa6f9767df5bad054
ericweidman <ericweidman@gmail.com>
2017-02-03 16:55:27 -0500

    Initial import.

---
+++ src/main/java/com/textdetective/TextDetectiveApplication.java
@@ -0,0 +1,12 @@
+package com.textdetective;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TextDetectiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TextDetectiveApplication.class, args);
+	}
+}
