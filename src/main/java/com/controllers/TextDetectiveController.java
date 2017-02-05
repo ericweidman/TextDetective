7915616b1063824ec2f9a88ca449d3aedea94e11
ericweidman <ericweidman@gmail.com>
2017-02-04 19:42:33 -0500

    Added more story stuff.

--- src/main/java/com/controllers/TextDetectiveController.java
+++ src/main/java/com/controllers/TextDetectiveController.java
@@ -23,23 +23,23 @@ public class TextDetectiveController {
     @RequestMapping(path = "/create-user", method = RequestMethod.POST)
     public void newUser(@RequestBody User newUser, HttpSession session) throws Exception {
 
-        User checkUser = users.findByUserName(newUser.getUserName());
-        User saveUser = new User(newUser.getUserName(), newUser.getPin());
+        User checkUser = users.findByUserName(newUser.getUserName().toLowerCase());
+        User saveUser = new User(newUser.getUserName().toLowerCase(), newUser.getPin());
         int userCount = (int) users.count() + 1;
 
         if (newUser.getUserName() == null) {
             throw new Exception("No username entered");
         } else if (checkUser == null) {
-            session.setAttribute("username", newUser.getUserName());
+            session.setAttribute("username", newUser.getUserName().toLowerCase());
             users.save(saveUser);
-            System.out.println("User " + newUser.getUserName() + " created!");
-            System.out.println("User count is now " + userCount+"!");
-        } else if (newUser.getUserName().equals(checkUser.getUserName()) && !newUser.getPin().equals(checkUser.getPin())) {
+            System.out.println("User " + newUser.getUserName().toLowerCase() + " created!");
+            System.out.println("User count is now " + userCount + "!");
+        } else if (newUser.getUserName().toLowerCase().equals(checkUser.getUserName()) && !newUser.getPin().equals(checkUser.getPin())) {
             throw new Exception("Invalid PIN");
-        } else if (newUser.getUserName().equals(checkUser.getUserName()) && newUser.getPin().equals(checkUser.getPin())) {
-            session.setAttribute("username", checkUser.getUserName());
+        } else if (newUser.getUserName().toLowerCase().equals(checkUser.getUserName()) && newUser.getPin().equals(checkUser.getPin())) {
+            session.setAttribute("username", checkUser.getUserName().toLowerCase());
         } else {
-            session.setAttribute("username", newUser.getUserName());
+            session.setAttribute("username", newUser.getUserName().toLowerCase());
             users.save(saveUser);
         }
 
@@ -52,13 +52,32 @@ public class TextDetectiveController {
     }
 
     @RequestMapping(path = "/intro", method = RequestMethod.GET)
-    public String intro() {
-        return "Sara Berkely found herself standing outside of a decidedly unremarkable Oakland home.</br>" +
+    public String intro(HttpSession session) {
+
+        String brian = (String) session.getAttribute("username");
+        String isBrian = brian.toLowerCase();
+
+        String hiBrian = "Hi Brian! I suspect you might be one of the only people who plays this.</br>" +
+                "So I wrote this switch statement specifically just to say thanks.</br>" +
+                "Thanks!</br></br>";
+
+        String intro = "Sara Berkeley found herself standing outside of a decidedly unremarkable Oakland home.</br>" +
                 "It was a typical California spring day, warm and beautiful.</br>Although she tended not to notice such things anymore.</br>" +
-                "She was here to do a job. A job she'd done for years, and one she was rather good at.</br>" +
-                "She was a detective for the Oakland Police Department. It was also a job she despised.</br>" +
+                "She is here to do a job. A job she'd done for years, and one she is rather good at.</br>" +
+                "She is a detective for the Oakland Police Department. It is also a job she despises.</br>" +
                 "She let out a long sigh. </br>\"Well, let's get this over with\" she muttered.</br></br>" +
                 "You may type 'help' at any time.";
+
+        switch (isBrian) {
+            case "brian":
+            case "brianweston":
+            case "mrchozo":
+            case "bweston":
+            case "brianw":
+                intro = hiBrian + intro;
+                break;
+        }
+        return intro;
     }
 
 
@@ -76,21 +95,63 @@ public class TextDetectiveController {
                                 "\"Look around.\"</br>" +
                                 "\"Move to (location)\" i.e. \"front door.\"</br>" +
                                 "\"Inspect (item)\" i.e. \"doorknob.\"</br>" +
-                                "\"Open (item)\" i.e. \"trashcan.\"</br></br>" +
+                                "\"Inventory\"</br>" +
+                                "\"Open (item)\" i.e. \"trashcan.\"</br>" +
+                                "\"About\"</br></br>" +
                                 "You are encouraged to experiment.</br>" +
                                 "---</br>";
                 break;
 
+            case "about":
+                response = "This game was created by Eric Weidman.</br>" +
+                        "If you're reading this, thank you so much for playing!</br>" +
+                        "If you have any questions/comments/feedback/criticisms/devjobs,</br>" +
+                        "I would love to hear from you! Shoot me an email ericweidman@gmail.com.</br>" +
+                        "All code for this game can be found at https://github.com/ericweidman/TextDetective</br>";
+                break;
+
             case "kill self":
             case "kill yourself":
-                response = "Sara considered killing herself for brief a moment. She decided now was not the time.";
+                response = "Sara considers killing herself for brief a moment. She decided now is not the time.";
+                break;
+
+            case "inventory":
+                response = "Sara has a keyring.";
+                break;
+
+            case "inspect keyring":
+            case "inspect keys":
+                response = "The keyring she is carrying used to belong to a criminal. There are two keys attached.</br>" +
+                        "One of the keys looks like any standard key. It probably unlocks a door.</br>" +
+                        "The other key has a large fob attached, displaying a FORD logo.</br>";
+                break;
+
+            case "open keys":
+            case "open keyring":
+                response = "Sara considers opening the key ring, but decides against it as there isn't much of a reason.";
                 break;
 
             case "look around":
-                response = "Sara looked around. She was standing on the sidewalk in an average Oakland neighborhood.</br>" +
-                        "Every nth house looked pretty much the same. Most of the driveways were empty. It was " +
-                        "the early afternoon on a Tuesday, presumably everyone was still at work.</br>" +
-                        "The house she was standing in front of she knew to be empty, it also looked as such.";
+                response = "Sara looks around. She is standing on the sidewalk in an average Oakland neighborhood.</br>" +
+                        "Every nth house looked pretty much the same. Most of the driveways were empty. It is " +
+                        "the early afternoon on a Tuesday, presumably everyone is still at work.</br>" +
+                        "The house closest to her she knows to be empty, it also looks as much.";
+                break;
+
+            case "open trashcan":
+                response = "There is no trashcan nearby for Sara to open.";
+                break;
+
+            case "inspect trashcan":
+                response = "There is no trashcan nearby for Sara to inspect.";
+                break;
+
+            case "inspect doorknob":
+                response = "Sara can hardly see the doorknob from where she is standing.</br>";
+                break;
+
+            case "open doorknob":
+                response ="Sara cannot reach the doorknob. She is getting a bit ahead of herself.";
                 break;
 
             case "move to front door":
