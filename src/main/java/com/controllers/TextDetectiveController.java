53e97a55dc7de25d6659a6d2b9ddaa0e80c8500c
ericweidman <ericweidman@gmail.com>
2017-02-06 01:11:31 -0500

    More changes

--- src/main/java/com/controllers/TextDetectiveController.java
+++ src/main/java/com/controllers/TextDetectiveController.java
@@ -63,13 +63,15 @@ public class TextDetectiveController {
 
         String intro = "Sara Berkeley finds herself standing in front of an unremarkable Oakland home.</br>" +
                 "Today is a typical California spring day. The sun is shining, birds are chirping, the air the perfect kind of mild.</br>" +
-                "She tends not to appreciate days like today anymore. Her world is constantly dampened by dark, ominous clouds.</br></br>" +
+                "She tends not to appreciate days like this one anymore. Her world is constantly dampened by dark, ominous clouds.</br></br>" +
 
-                "Sara suffers from major depression disorder. Her doctor diagnosed her recently, but she knew it had been her truth for the majority of her cognitive years.</br>" +
-                "Oftentimes she thinks about how hard she fought it when she was younger.</br>" +
-                "She is completely aware that she doesn't try to compete with it anymore. She is outmatched. Some days she almost embraces it.</br></br>"+
+                "Sara suffers from major depressive disorder. Her doctor diagnosed her recently, but it has been her truth for the majority of her days.</br>" +
+                "Oftentimes she thinks about how hard she would fight it when she was younger. Sometimes she wonders what changed.</br>" +
+                "She is completely aware that she doesn't try to compete with it anymore. She's been outmatched for months.</br> " +
+                "Some days she refuses to grapple her depression, and instead embraces it.</br>" +
+                "Today is one of those days.</br></br>"+
 
-                "She is at this particular house because she has her job to do. The same job she's been doing for years. A job she has always excelled at.</br>" +
+                "She is at this particular house because she has her job to do. The same job she's been doing for years.</br>" +
                 "She is a detective for the Oakland City Police Department.</br></br>" +
 
                 "Sara despises her work.</br></br>" +
@@ -78,7 +80,7 @@ public class TextDetectiveController {
                 "\"Well, let's get this over with.\" she muttered.</br></br>" +
                 "---</br></br>" +
 
-                "Enter \"commands\" for a list of actions to help get you started.</br></br>" +
+                "Enter \"commands\" at any time to see a list of useful actions.</br></br>" +
                 "---</br>";
 
         switch (isBrian) {
@@ -109,11 +111,11 @@ public class TextDetectiveController {
                 response =
                         "---<br>" +
                                 "Useful commands include things like:</br></br>" +
-                                "\"Look around.\"</br>" +
-                                "\"Move to (location)\" i.e. \"front door.\"</br>" +
-                                "\"Inspect (item)\" i.e. \"doorknob.\"</br>" +
+                                "\"Look around\"</br>" +
+                                "\"Move to (location)\" e.g. \"front door\"</br>" +
+                                "\"Inspect (item)\" e.g. \"doorknob\"</br>" +
                                 "\"Inventory\"</br>" +
-                                "\"Open (item)\" i.e. \"trashcan.\"</br>" +
+                                "\"Open (item)\" e.g. \"trashcan\"</br>" +
                                 "\"About\"</br></br>" +
                                 "You are encouraged to experiment.</br></br>" +
                                 "Enter \"save\" to save and quit.</br>" +
@@ -126,11 +128,11 @@ public class TextDetectiveController {
             case "about detective sara":
                 response = "You are currently logged in as " + userName + ".</br>" +
                         "---</br>" +
-                        "Detective Sara was programmed and written by Eric Weidman.</br>" +
-                        "If you're reading this, thank you for playing!</br>" +
-                        "If you have any questions/comments/feedback/criticisms/devjobs,</br>" +
-                        "I would love to hear from you! Shoot me an email - ericweidman@gmail.com.</br>" +
-                        "All code for this game can be found at https://github.com/ericweidman/TextDetective</br>";
+                        "'Detective Sara' was programmed and written by Eric Weidman.</br>" +
+                        "If you have any questions/comments/feedback/criticisms/devjobs I would love to hear form you!</br>" +
+                        "Feel free to shoot me an email - ericweidman@gmail.com</br>" +
+                        "All code for this game can be found at https://github.com/ericweidman/TextDetective</br>" +
+                        "If you're reading this, thank you for playing!</br>";
 
                 break;
 
@@ -151,6 +153,8 @@ public class TextDetectiveController {
                 response = "Sara considers hurting herself. She thinks there will be plenty of time for self loathing later.";
                 break;
 
+            case "inspect inventory":
+            case "open inventory":
             case "item":
             case "inventory":
             case "items":
@@ -159,7 +163,7 @@ public class TextDetectiveController {
 
             case "inspect keyring":
             case "inspect keys":
-                response = "The keyring she is carrying used to belong to John Mercer. There are two keys attached.</br>" +
+                response = "The keyring she is carrying used to belong to Johnathan Mercer. There are two keys attached.</br>" +
                         "One of the keys looks like any standard key. It probably unlocks a door.</br>" +
                         "The other key has a large fob attached, displaying a FORD logo.";
                 break;
@@ -185,13 +189,13 @@ public class TextDetectiveController {
                 response = "Sara looks around. She is standing on the sidewalk in an average Oakland neighborhood.</br>" +
                         "It's the early afternoon on a Tuesday. Most of the driveways are empty.</br> " +
                         "The normal folk who live here are at work.</br>" +
-                        "All of the houses are rather plain looking, every nth house or so are seemingly identical.</br>"+
-                        "The uninteresting house closest to her, she knows to be empty. Her cruiser is parked in the driveway.</br>" +
-                        "What is left of her sense of duty beckons her to the front door.";
+                        "All of the houses are rather plain looking. Every nth house or so is seemingly identical.</br>"+
+                        "The uninteresting house closest to her, she suspects to be empty. Her cruiser is parked in the driveway.</br>" +
+                        "What's left of her sense of duty pushes her to the front door.";
                 break;
 
             case "inspect":
-                response = "Sara wonders what kind of things she'll get to inspect today.";
+                response = "Sara wonders what kind of of horrors she'll have to inspect today.";
                 break;
 
             case "open":
@@ -238,20 +242,20 @@ public class TextDetectiveController {
                 response = "As Sara walks towards the door, she starts to think about Johnathan Mercer.</br>" +
                         "Johnathan suffocated himself earlier this morning during his latest stint in a city jail cell.</br>" +
                         "Apparently somebody had forgotten to remove his belt before locking him up for the night.</br>" +
-                        "He did it shorty after a search warrant for his house had been issued.</br>" +
-                        "The same house Sara was headed to conduct a search on.</br><br>" +
+                        "He did it shorty after a search warrant for his home had been issued.</br>" +
+                        "The same home Sara was headed to.</br><br>" +
 
                         "She thought the suicide a bit odd, Johnathan had been in and out the county jail more times than she could remember.</br>" +
                         "His slimy lawyer normally didn't have any trouble getting him out.</br>" +
                         "Typically one of her idiot coworkers would forget how to book him properly, or forget to read him his rights,</br>" +
                         " or beat the ever loving shit out of him before bringing him in.</br></br>" +
 
-                        "Her fellow officers didn't give the suicide a second thought, in fact they celebrated it.</br>" +
-                        "While she didn't have any respect for Johnathan, she saw no reason to celebrate another addition to" +
+                        "Her fellow officers didn't give the suicide a second thought, in fact many of them celebrated it.</br>" +
+                        "While she didn't have any good things to say about Johnathan, she saw no reason to celebrate another addition to" +
                         " the long list of her coworkers fuck-ups.</br>" +
-                        "She hates everyone in her precinct.</br></br>" +
+                        "She dislikes everyone at her precinct.</br></br>" +
 
-                        "Her final thought before she reached the door was \"lucky bastard.\"</br>"+
+                        "Her final thought concerning Johnathan before reaching the door was \"lucky bastard...\"</br>"+
                         "She chuckled to herself.";
                 break;
 
@@ -274,6 +278,11 @@ public class TextDetectiveController {
             case "sara sucks":
             case "detective sara sucks":
                 response = "\"No, you suck.\" Sara suddenly quipped for some reason that even baffled her...";
+                break;
+
+            case "what is my username":
+            case "username":
+                response = "Your username is " + userName + ".";
                 break;
 
             default:
